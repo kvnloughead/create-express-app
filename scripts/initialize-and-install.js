0fbd3a94177102fbbef9c62e739671d0f2bdd304
Kevin Loughead <kvnloughead@gmail.com>
2022-10-25 19:37:28 -0400

    Update contents of .gitignore

--- scripts/initialize-and-install.js
+++ scripts/initialize-and-install.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { installDependencies } = require('../utils');
 const { dependencies, devDependencies } = require('../utils/constants');
 
-const gitignorables = ['node_modules', '.DS_Store'];
+const gitignorables = `# Logs\nlogs\n*.log\nnpm-debug.log*\n\n# Dependency directory\nnode_modules\n\n# Optional npm cache directory\n.npm\n\n# System files and IDE settings folders\n.DS_Store\n.idea\n.vscode`;
 
 const nodeInitialization = (options) => `
   mkdir ${options.projectName}
@@ -16,7 +16,7 @@ const setUpGit = (options) => `
   cd ${options.projectName}
   git init
   touch .gitignore
-  echo "${gitignorables.join('\n')}" >> .gitignore
+  echo "${gitignorables}" >> .gitignore
 `;
 
 const installPackages = (options) => `
